b81496d1d81ccb9a2e3bef4efb96f3f8abeab3b4
Sumit Banik <sumitbanik@groveus.com>
2020-05-13 15:45:06 +0530

    Fixed State Data Bugs and Added Delta Changes

--- data.py
+++ data.py
@@ -21,6 +21,11 @@ class Country:
             class_='field field-name-field-migrated-counts field-type-text field-label-above')).text[10:]
         death_cases = (soup.find(
             class_='field field-name-field-total-death-case field-type-number-integer field-label-above')).text[8:]
+
+        last_total_active_cases = soup.find(class_="field field-name-field-last-total-active field-type-number-integer field-label-above").text.split('\xa0')[1]
+        last_total_recovered_cases = soup.find(class_="field field-name-field-last-total-cured field-type-number-integer field-label-above").text.split('\xa0')[1]
+        last_total_death_cases = soup.find(class_="field field-name-field-last-total-death field-type-number-integer field-label-above").text.split('\xa0')[1]
+
         country_data = {
             'last_updated': last_updated,
             'passengers_screened': int(passengers_screened.replace(",","")),
@@ -31,14 +36,19 @@ class Country:
             'confirmed_cases':int(active_cases)+int(recovered_cases)+int(death_cases),
             'death_rate': round((int(death_cases)/(int(active_cases)+int(recovered_cases)+int(death_cases))*100),2),
             'active_rate': round((int(active_cases)/(int(active_cases)+int(recovered_cases)+int(death_cases))*100),2),
-            'recovered_rate': round((int(recovered_cases)/(int(active_cases)+int(recovered_cases)+int(death_cases))*100),2)
+            'recovered_rate': round((int(recovered_cases)/(int(active_cases)+int(recovered_cases)+int(death_cases))*100),2),
+            'last_total_active_cases':int(last_total_active_cases),
+            'last_total_recovered_cases':int(last_total_recovered_cases),
+            'last_total_death_cases':int(last_total_death_cases),
+            'delta_change_active_cases':int(active_cases)-int(last_total_active_cases),
+            'delta_change_recovered_cases':int(recovered_cases)-int(last_total_recovered_cases),
+            'delta_change_death_cases':int(death_cases)-int(last_total_death_cases)
 
         }
         return country_data
 
     def state_data(self):
         URL = 'https://www.mygov.in/corona-data/covid19-statewise-status'
-
         page_content = rq.get(URL).content
         soup = BeautifulSoup(page_content, 'html.parser')
         datas = (soup.find(
@@ -49,26 +59,32 @@ class Country:
             if data:
                 state_name = data.split('\xa0')[1].split('Total')[0]
                 confirmed_case = data.split('\xa0')[2].split('Cured')[0]
-                cured = data.split('\xa0')[3].split('Death')[0]
-                death = data.split('\xa0')[4]
-                st_data = {
-                    'state': state_name,
-                    'confirmed_cases': int(confirmed_case),
-                    'cured_cases': int(cured),
-                    'death_cases': death,
-                    'active_cases':int(confirmed_case)-int(death)-int(cured),
-                    'active_rate':round(((int(confirmed_case)-int(death)-int(cured))/int(confirmed_case)*100),2),
-                    'death_rate':round((int(death)/int(confirmed_case)*100),2),
-                    'recovered_rate':round((int(cured)/int(confirmed_case)*100),2)
+                recovered = data.split('\xa0')[3].split('Death')[0]
+                death = data.split('\xa0')[4][:-11]
+                if(int(confirmed_case)==0):
+                    active_r=recovered_r=death_r = 0
+                else:
+                    active_r = round((int(confirmed_case)-int(recovered)-int(death))/int(confirmed_case)*100,2)
+                    recovered_r = round(int(recovered)/int(confirmed_case)*100,2)
+                    death_r = round(int(death)/int(confirmed_case)*100,2)
+                data_state = {
+                    'state': str(state_name),
+                    'confirmed': int(confirmed_case),
+                    'recovered': int(recovered),
+                    'deaths': int(death),
+                    'active':int(confirmed_case)-int(recovered)-int(death),
+                    'active_rate':active_r,
+                    'recovered_rate':recovered_r,
+                    'death_rate':death_r
                 }
-                json_data.append(st_data)
+                json_data.append(data_state)
 
         state_data={
             'source':'myGov',
             'state_data':json_data
         }
-        print(state_data)
         return state_data
+
 
 
     def covidTestCount(self):
